90e0ad19435de5c6b5d193da632ddddeed448c52
Saroj Shrestha <saroj.shr911@gmail.com>
2017-05-11 12:17:53 +0545

    basic set up

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return "Hi there, how ya doin?"
+
+
+if __name__ == "__main__":
+    app.run()
+#app.run(host='0.0.0.0', port=8080)
+
